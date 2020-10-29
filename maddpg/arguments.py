31f69c7ffdd9bc2524e8f19859472f594c3bc666
iminders <liuwen.w@qq.com>
2020-10-29 19:59:26 +0800

    explore

--- maddpg/arguments.py
+++ maddpg/arguments.py
@@ -28,7 +28,7 @@ def parse_experiment_args():
                         help="learning rate for Adam optimizer")
     parser.add_argument("--gamma", type=float, default=0.95,
                         help="discount factor")
-    parser.add_argument("--sigma", type=float, default=0.8,
+    parser.add_argument("--sigma", type=float, default=0.85,
                         help="explore noise sigma")
     parser.add_argument("--decay_rate", type=float, default=0.98,
                         help="explore noise sigma decay rate")
