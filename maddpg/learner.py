52540db4e18a00c6772ad0429a58aab95a8eadef
iminders <liuwen.w@qq.com>
2020-11-01 11:40:02 +0800

    gc

--- maddpg/learner.py
+++ maddpg/learner.py
@@ -57,7 +57,6 @@ def serve(agent):
     with agent.writer.as_default():
         while True:
             z = s.recv_pyobj()
-            print(len(z))
             p = zlib.decompress(z)
             data = pickle.loads(p)
             [obs, action, next_obs, rew, done, terminal] = data
