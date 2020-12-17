4332e4aae3184d35f417c3d5b46eaed49fa19f2b
iminders <liuwen.w@qq.com>
2020-12-17 09:51:49 +0800

    warm_up

--- starter/docker_run.sh
+++ starter/docker_run.sh
@@ -7,5 +7,5 @@ cd /root/maddpg
 pip install -e .
 
 python -m maddpg.run --save_rate=1000 --num_env 10  --env_batch_size 100 \
-    --warm_up 1500 --num_agent $(num_agent) \
+    --warm_up 3500 --num_agent $(num_agent) \
     --enable_prioritized_replay
